1f9feaacf8f664ec7d735e8dc425a8bf2883353c
Jesse Barton <jesse.barton@life.church>
2018-08-30 11:27:45 -0500

    web sockets started

--- main.go
+++ main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func main() {
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
+
+		for {
+			// Read message from browser
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				log.Fatalf("%v", err)
+			}
+
+			// Print the message to the console
+			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+
+			// Write message back to browser
+			if err = conn.WriteMessage(msgType, msg); err != nil {
+				log.Fatalf("%v", err)
+			}
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
